43fdfd1ba57e83d71382e584e8fad52c8e438410
Davis Robertson <EpicDaviBrickFilms@gmail.com>
2014-04-18 23:09:43 -0500

    Adding "Carousel" and Fixing Nav
    Adding a pseudo-carousel and fixing some things in the nav.

--- js/datScript.js
+++ js/datScript.js
@@ -1,3 +1,44 @@
-$(document).ready(function(){
-    $("#test").hide();
+var hash = window.location.hash;
+
+$(document).ready(function() {
+    $(".page").hide();
+    comeFromRight($(".page:first"));
+    $(".page").click(function(){
+        fadeToLeft($(this));
+        comeFromRight($(this).next());
+        $(this).appendTo("#pages");
+    });
 });
+
+function fadeToLeft(elem){
+    elem.animate({
+        left: "-150%",
+        opacity: "0"
+    },750,function(){
+        elem.css({
+            left: "150%"
+        });
+        elem.hide();
+    });
+
+}
+
+function comeFromRight(elem){
+    elem.show();
+    elem.animate({
+        left: "0%",
+        opacity: "1"
+    },750);
+}
+
+function fadeCurthenSpec(elid){
+    var $fp = $(".page:first");
+    if($fp.attr("id")!=elid){
+        fadeToLeft($fp);
+        $fp.appendTo("#pages");
+        while($(".page:first").attr("id")!=elid){
+            $(".page:first").appendTo("#pages");
+        }
+        comeFromRight($("#"+elid));
+    }
+}
