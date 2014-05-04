c72f5b6014b30a80e95e40e52826f0a9ce8d878a
Davis Robertson <EpicDaviBrickFilms@gmail.com>
2014-05-03 23:54:42 -0500

    Pages Moving and Google Stuff
    Made functions for moving pages other directions. Many bugs right now,
    so haven't implemented fully. Also adding stuff for Google Webmaster
    Stuff.

--- js/datScript.js
+++ js/datScript.js
@@ -1,8 +1,6 @@
-var hash = window.location.hash;
-
 $(document).ready(function() {
     $(".page").hide();
-    comeFromRight($(".page:first"));
+    comeFromRight($("#pages .page:first"));
     $(".page").click(function(){
         fadeToLeft($(this));
         comeFromRight($(this).next());
@@ -10,33 +8,88 @@ $(document).ready(function() {
     });
 });
 
-function fadeToLeft(elem){
+function shiftLeft(){
+    var $pf = $("#pages .page:first");
+    var fa=false, sa=false;
+    fadeToLeft($pf, function(){
+        fa=true;
+    });
+    comeFromRight($pf.next(),function(){
+        ifNoneShow($("#pages"));
+        $pf.appendTo("#pages");
+    });
+}
+
+function ifNoneShow(holder){
+    if(holder.firstChild.is(":visible")){
+        holder.firstChild.show();
+    }
+}
+
+function fadeToLeft(elem, cb){
+    cb=cb!==undefined?cb:function(){};
     elem.animate({
         left: "-150%",
         opacity: "0"
     },750,function(){
-        elem.css({
-            left: "150%"
-        });
         elem.hide();
+        elem.css({
+            left: "0%"
+        });
+        cb();
     });
 
 }
 
-function comeFromRight(elem){
+function comeFromLeft(elem, cb){
+    cb=cb!==undefined?cb:function(){};
+    elem.css({
+        left: "-150%"
+    });
     elem.show();
     elem.animate({
         left: "0%",
         opacity: "1"
-    },750);
+    },750, function(){
+        elem.show();
+        cb();
+    });
+}
+
+function fadeToRight(elem, cb){
+    cb=cb!==undefined?cb:function(){};
+    elem.show();
+    elem.animate({
+        left: "150%",
+        opacity: "0"
+    },750,function(){
+        elem.hide();
+        elem.css({
+            left: "0%"
+        });
+        cb();
+    });
+
+}
+
+function comeFromRight(elem, cb){
+    cb=cb!==undefined?cb:function(){};
+    elem.css({
+        left: "150%"
+    });
+    elem.show();
+    elem.animate({
+        left: "0%",
+        opacity: "1"
+    },750,cb);
 }
 
 function fadeCurthenSpec(elid){
-    var $fp = $(".page:first");
+    var $fp = $("#pages .page:first");
     if($fp.attr("id")!=elid){
         fadeToLeft($fp);
         $fp.appendTo("#pages");
-        while($(".page:first").attr("id")!=elid){
+        while($("#pages .page:first").attr("id")!=elid){
             $(".page:first").appendTo("#pages");
         }
         comeFromRight($("#"+elid));
